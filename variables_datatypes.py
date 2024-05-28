f72f708ec31a91e6ce005d35af64c9ac87e3c4d8
Tommy Truong <ttruong@emergejobs.com>
2024-05-28 06:18:09 -0400

    First commit with existing lesson examples.

---
+++ variables_datatypes.py
@@ -0,0 +1,25 @@
+'''
+
+This program now includes examples of each data type: string, integer, float, list, set, range, dictionary, and boolean.
+Each variable is initialized with the corresponding data  type and example values.
+When executed, the program prints the values of these variables to demonstrate how they store different types of data in Python.
+
+'''
+
+# Define variables of different data types
+player_name = "Hero"    # String data type
+health_points = 100     # Integer data type
+mana_points = 75.5      # Float data type
+inventory = ["sword", "shield", "potion"]   # List data type
+unique_numbers = {1, 2, 3, 4, 5}    # Set data type
+player_info = {"name": player_name, "level": 5, "silver": 50, "gold": 20} # Dictionary data type
+is_alive = True         # Boolean data type
+
+# Print the values of the variables
+print("Player Name:", player_name)
+print("Health Points:", health_points)
+print("Mana Points:", mana_points)
+print("Inventory:", inventory)
+print("Unique Numbers:", unique_numbers)
+print("Player Info:", player_info)
+print("Is Alive:", is_alive)
